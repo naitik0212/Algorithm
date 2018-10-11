3b9e996847f733a0b728a41f12d866a891684f64
naitik0212 <shahnaitik0212@gmail.com>
2018-10-11 10:12:17 -0700

    arraycopy

---
+++ src/arrays/assignArray.java
@@ -0,0 +1,27 @@
+package arrays;
+
+//overwrites with the latest array copied.
+
+public class assignArray {
+
+
+        public static void main(String[] args)
+        {
+            int[] arrayWitThree = {1,2,3} ;
+
+            int[] arrayWithfive = {4,5,6,7,8};
+            arrayWithfive = arrayWitThree;
+
+            System.out.println("Array with 3 elements:" );
+            for(int i : arrayWitThree){
+                System.out.println(i);
+            }
+            System.out.println("Array with 5 elements:" );
+            for(int i : arrayWithfive){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
