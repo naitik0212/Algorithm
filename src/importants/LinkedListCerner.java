77508f783bd6919d196451d91d90ac3a52f72b9d
naitik0212 <shahnaitik0212@gmail.com>
2019-02-18 19:07:11 -0700

    cerner prep

--- src/importants/LinkedListCerner.java
+++ src/importants/LinkedListCerner.java
@@ -1,17 +1,111 @@
 package importants;
 
-import java.util.LinkedList;
+//import java.util.LinkedList;
+import java.io.*;
 
-public class LinkedListCerner {
-    public static void main(String args[]){
-        LinkedList<Character> list = new LinkedList<>();
-        list.add('C');
-        list.add('E');
-        list.add('R');
-        list.add('N');
-        list.add('E');
-        list.add('R');
 
-        System.out.print(list);
+//public class LinkedListCerner {
+//    public static void main(String args[]){
+//        LinkedList<Character> list = new LinkedList<>();
+//        list.add('C');
+//        list.add('E');
+//        list.add('R');
+//        list.add('N');
+//        list.add('E');
+//        list.add('R');
+//
+//        System.out.print(list);
+//    }
+//}
+//
+
+
+// Java program to implement
+// a Singly Linked List
+class LinkedList {
+
+    Node head; // head of list
+
+    // Linked list Node.
+    // This inner class is made static
+    // so that main() can access it
+    static class Node {
+
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Method to insert a new node
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        // Create a new node with given data
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        // If the Linked List is empty,
+        // then make the new node as head
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            // Else traverse till the last node
+            // and insert the new_node there
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            // Insert the new_node at last node
+            last.next = new_node;
+        }
+
+        // Return the list by head
+        return list;
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(LinkedList list)
+    {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        // Traverse through the LinkedList
+        while (currNode != null) {
+            // Print the data at current node
+            System.out.print(currNode.data + " ");
+
+            // Go to next node
+            currNode = currNode.next;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+		/* Start with the empty list. */
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 'C');
+        list = insert(list, 'E');
+        list = insert(list, 'R');
+        list = insert(list, 'N');
+        list = insert(list, 'E');
+        list = insert(list, 'R');
+
+        // Print the LinkedList
+        printList(list);
     }
 }
